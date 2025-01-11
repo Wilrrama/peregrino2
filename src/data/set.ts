e18a15d2b93f5e673d5e2b249fee29297093800e
wilrrama <wilrrama@hotmail.com>
2025-01-11 08:36:13 -0300

    feat: Setembro

--- src/data/set.ts
+++ src/data/set.ts
@@ -2,15 +2,14 @@ import recreio from "../assets/img/a2.jpg";
 import amanda1 from "../assets/img/A1.jpg";
 import amanda4 from "../assets/img/a4.jpg";
 
-import audio_01_11 from "../assets/audios/nov/01-11.opus";
-import audio_11_11 from "../assets/audios/nov/11-11.opus";
-import audio_15_11 from "../assets/audios/nov/15-11.opus";
-import audio_17_11 from "../assets/audios/nov/17-11.opus";
-import audio_18_11 from "../assets/audios/nov/18-11.opus";
-import audio_25_11 from "../assets/audios/nov/25-11.opus";
-import audio_29_11 from "../assets/audios/nov/29-11.opus";
+import audio0609 from "../assets/audios/set/06-09.opus";
+import audio0909 from "../assets/audios/set/09-09.opus";
+import audio1309 from "../assets/audios/set/13-09.opus";
+import audio2009 from "../assets/audios/set/20-09.opus";
+import audio2309 from "../assets/audios/set/23-09.opus";
+import audio2909 from "../assets/audios/set/29-09.opus";
 
-export interface INovItem {
+export interface ISetItem {
   id: number;
   img: string;
   data: string;
@@ -22,57 +21,49 @@ export const set = [
   {
     id: 1,
     img: amanda4,
-    data: "01/11/2024",
+    data: "06/09/2024",
     nome: "",
-    word: "",
-    audio: audio_01_11,
+    word: "Felipense cap4 v10",
+    audio: audio0609,
   },
   {
     id: 2,
     img: recreio,
-    data: "11/11/2024",
+    data: "09/09/2024",
     nome: "",
-    word: "Daniel cap6",
-    audio: audio_11_11,
+    word: "Êxodo cap17 v8",
+    audio: audio0909,
   },
   {
     id: 3,
     img: amanda4,
-    data: "15/11/2024",
+    data: "13/09/2024",
     nome: "",
-    word: "Atos apóstolos  cap 9",
-    audio: audio_15_11,
+    word: "Salmos 40",
+    audio: audio1309,
   },
   {
     id: 4,
-    img: amanda1,
-    data: "17/11/2024",
-    nome: "Santa Isabel rjm",
-    word: "Filemon cap9",
-    audio: audio_17_11,
+    img: amanda4,
+    data: "20/09/2024",
+    nome: "",
+    word: "",
+    audio: audio2009,
   },
   {
     id: 5,
     img: recreio,
-    data: "18/11/2024",
+    data: "23/09/2024",
     nome: "",
-    word: "2 reis cap2 v8",
-    audio: audio_18_11,
+    word: "Ester cap5",
+    audio: audio2309,
   },
   {
     id: 6,
-    img: recreio,
-    data: "25/11/2024",
+    img: amanda1,
+    data: "29/09/2024",
     nome: "",
-    word: "Josué cap 2",
-    audio: audio_25_11,
-  },
-  {
-    id: 7,
-    img: amanda4,
-    data: "29/11/2024",
-    nome: "",
-    word: "",
-    audio: audio_29_11,
+    word: "Josué Cap1 v9",
+    audio: audio2909,
   },
 ];
